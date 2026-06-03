7aa3407cd7a4c16c0b12a1362617b22166dad323
armaan535kh <kh.armaan535@gmail.com>
2026-06-03 03:12:36 -0700

    [Update] updaing the sideTriangle

--- Self_Learning/sideTriangle.py
+++ Self_Learning/sideTriangle.py
@@ -1,17 +1,24 @@
 #program to check three sides form a triangle or not
 #Input from the user
 
+#Validation
 side1 = int(input("Enter the side 1 : "))
 if(side1 <= 0):
     exit("Side should be positive")
+
+#-----------------------------------------
 
 side2 = int(input("Enter the side 2 : "))
 if(side2 <= 0):
     exit("Side2 should be positive")
 
+
+#-------------------------------------------
 side3 = int(input("Enter the side 3 : "))
 if(side3 <= 0):
     exit("Side3 should be positive")
+
+#-------------------------------------------
 
 if(side1 + side2 > side3 and side2 + side3 > side1 and side3 + side1 > side2):
     print("Three sides form a triangle")
