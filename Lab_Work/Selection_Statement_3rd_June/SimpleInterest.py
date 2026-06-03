30fa2a21a94c5a2f076c0d8a9b4dd207ffaef8d0
armaan535kh <kh.armaan535@gmail.com>
2026-06-03 02:30:41 -0700

    [Update] updating simple interest

--- Lab_Work/Selection_Statement_3rd_June/SimpleInterest.py
+++ Lab_Work/Selection_Statement_3rd_June/SimpleInterest.py
@@ -1,14 +1,21 @@
 #Program to calculate Simple interest
 #Input from the user rate, interest and principal
 
+
+#checking the rate
 rate = int(input("Enter the rate : "))
+if(rate < 0):
+    exit("Rate can't be negative")
+
+#checking the time
 time = int(input("Enter the time : "))
+if(time < 0):
+    exit("Time can't be negative")
+
+#checking the principal
 principal = int(input("Enter the princpal : "))
-
-#check the input
-
-if(rate < 0 and principal < 0):
-    exit("Rate and Principal can't be negative")
+if(principal < 0):
+    exit("Principal can't be negative")
 
 print("-----------------------------------------")
 
