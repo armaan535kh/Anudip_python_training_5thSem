3f61a1cc26e6306ccf6a0614b0fa059cb98f4577
armaan535kh <kh.armaan535@gmail.com>
2026-06-04 02:04:50 -0700

    [Add] strong_number_verification

---
+++ Lab_Work/iteration_4th_june_lab/strong_number_verification.py
@@ -0,0 +1,25 @@
+#Program to check whether a given number is a Strong Number. 
+
+# Input number
+number = int(input("Enter the number : "))
+
+temp = number
+digit_sum = 0
+
+# Loop through each digit of the number
+while temp > 0:
+    digit = temp % 10
+    
+    # Calculate the factorial 
+    fact = 1
+    for i in range(1, digit + 1):
+        fact *= i
+        
+    digit_sum += fact
+    temp //= 10
+
+
+if digit_sum == number:
+    print(f"{number} is a Strong Number")
+else:
+    print(f"{number} is not a Strong Number")
