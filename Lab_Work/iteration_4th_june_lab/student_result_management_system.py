c455a859bb8360619db63c01dbdc896af6eac46e
armaan535kh <kh.armaan535@gmail.com>
2026-06-04 02:54:43 -0700

    [add] student_result_management

---
+++ Lab_Work/iteration_4th_june_lab/student_result_management_system.py
@@ -0,0 +1,40 @@
+# Program to Accept marks of 5 subjects.
+
+# Student Result Management System 
+
+total_marks = 0.0
+failed_subjects_count = 0
+
+# take input and calculate sums
+for i in range(1, 6):
+    print("Enter marks for Subject", i, ":")
+    score = float(input("Enter the Marks : "))
+    
+    # Accumulate the total marks
+    total_marks = total_marks + score
+    
+    #Check if the individual subject is failed
+    if score < 40:
+        failed_subjects_count = failed_subjects_count + 1
+
+
+percentage = total_marks / 5
+
+
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 75:
+    grade = "A"
+elif percentage >= 60:
+    grade = "B"
+elif percentage >= 40:
+    grade = "C"
+else:
+    grade = "Fail"
+
+# Display Outputs
+print("\n--- Student Result Summary ---")
+print("Total Marks:", total_marks, "/ 500")
+print("Percentage:", percentage, "%")
+print("Final Grade:", grade)
+print("Number of Subjects Failed:", failed_subjects_count)
